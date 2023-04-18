f068ff5f2e47f50130567149bdeb66c52aaf974d
Ariana Ortiz <ariortizleon@gmail.com>
2023-04-18 16:15:57 -0500

    Ejercicios sobre operaciones

---
+++ Ejercicio-5.js
@@ -0,0 +1,50 @@
+
+var a=2,b=3,c=4;
+
+//Punto 4
+var operacion1 = (a*b)/c;
+var operacion2 = (b+c)*a;
+var operacion3 = (a/b)-(a+c);
+
+console.log("El resultadod e la operacion 1 es " , operacion1);
+console.log("El resultadod e la operacion 2 es " , operacion2);
+console.log("El resultadod e la operacion 3 es " ,operacion3);
+
+//operador modulo
+
+var modulo = c%a;
+
+console.log("El resultado de modulo entre", c, " y" , a, "es " ,modulo);
+
+//Incremento de más de una unidad;
+
+
+
+
+//Tabla de verdad 
+let verdadero = true;
+let falso = false;
+
+//AND
+var condicion1 = verdadero && verdadero
+var condicion2 = verdadero && falso
+var condicion3 = falso && verdadero
+var condicion4 = falso && falso
+
+console.log(condicion1)
+console.log(condicion2)
+console.log(condicion3)
+console.log(condicion4)
+
+//OR
+
+var condicion1 = verdadero || verdadero
+var condicion2 = verdadero || falso
+var condicion3 = falso || verdadero
+var condicion4 = falso || falso
+
+console.log(condicion1)
+console.log(condicion2)
+console.log(condicion3)
+console.log(condicion4)
+
