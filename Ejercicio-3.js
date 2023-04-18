720b1df399af0baee79db57e37fcf3ad3db3e7c4
Ariana Ortiz <ariortizleon@gmail.com>
2023-04-18 15:35:45 -0500

    Ejercicio de constantes

---
+++ Ejercicio-3.js
@@ -0,0 +1,10 @@
+const PI=3.1416
+//Otra manera de concatenar texto es: `Texto ${variable / constante}`
+console.log(`El valor de PI es: ${PI}`)
+
+//Ejercicio propuesto en la guía
+const segInMin = 60;
+const minInHour = 60;
+const HourInDay = 24
+
+console.log("Un minuto tiene ", segInMin," segundos, una hora tiene ", minInHour, " minutos y un día tiene ", HourInDay, " horas");
