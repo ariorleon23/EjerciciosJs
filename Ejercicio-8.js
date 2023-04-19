efd9cdc8aca819f15206d44cff0f6f5e64e2939e
Ariana Ortiz <ariortizleon@gmail.com>
2023-04-19 09:03:55 -0500

    Ejercicios de funciones

---
+++ Ejercicio-8.js
@@ -0,0 +1,15 @@
+
+function OperacionesMatematicas(num1,num2){
+    suma = num1+num2;
+    resta = num1-num2;
+    multiplicacion = num1*num2;
+    division = num1/num2;
+
+    console.log(`La suma entre ${num1} y ${num2} da como resultado ${suma}.`)
+    console.log(`La resta entre ${num1} y ${num2} da como resultado ${resta}.`)
+    console.log(`La multiplicación entre ${num1} y ${num2} da como resultado ${multiplicacion}.`)
+    console.log(`La división entre ${num1} y ${num2} da como resultado ${division}.`)
+
+}
+
+OperacionesMatematicas(10,5);
