9ddc284e5fe7044fe30f3b13d605b5684fb35f16
Ariana Ortiz <ariortizleon@gmail.com>
2023-04-24 16:20:13 -0500

    Ejerciccios de funciones expresadas y autodeclaradas

---
+++ Ejercicio-9.js
@@ -0,0 +1,32 @@
+//Funcion expresada
+const relacion = function(a,b){
+    if(a>b){
+        return 1;
+    }
+    else if(a<b){
+        return -1;
+    }
+    else {
+        return 0;
+    }
+}
+
+console.log(relacion(10,10))
+
+//Funcion autodeclarada Realizar un código con funciones anónimas autoejecutables realice un algoritmo que diga si un
+//número entero positivo es par o no. Utilice la operación módulo.
+
+(function (num1){
+    if(num1%2==0)
+    {
+    console.log(`El número ${num1} es par`);
+    }
+    else{
+        console.log(`El número ${num1} es impar`)
+    }
+   
+})(0)
+
+
+
+
