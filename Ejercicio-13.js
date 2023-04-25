070f4d3239edc5f5dff93dc7036f501b6896f870
Ariana Ortiz <ariortizleon@gmail.com>
2023-04-25 09:21:47 -0500

    Ejercicio de array

---
+++ Ejercicio-13.js
@@ -0,0 +1,7 @@
+const peliculas = ["La milla verde","Parásitos","El viaje de Chihiro","Coco","Interestelar"];
+
+for(i=0; i<peliculas.length;i++){
+    console.log(`Película #${i+1}:${peliculas[i]}`)
+}
+
+
